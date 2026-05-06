5209fd895c4b6cc37b609dd7d44b02f54fc45ccf
omarkafi-554 <omar.kafi.2017@gmail.com>
2026-05-06 17:07:23 +0300

    Update prepare_uwf.py

--- scripts/prepare_uwf.py
+++ scripts/prepare_uwf.py
@@ -1,11 +1,9 @@
 import pandas as pd
 
-# تحميل dataset
 df1 = pd.read_csv("data/external/uwf_conn.csv")
 df2 = pd.read_csv("data/external/uwf_conn2.csv")
 
 df = pd.concat([df1, df2])
-# اختيار الأعمدة المهمة
 df = df[[
     "duration",
     "orig_pkts",
@@ -15,19 +13,16 @@ df = df[[
     "mitre_attack_tactics"
 ]]
 
-# حذف القيم الفارغة
 df = df.dropna()
 
-# 🧠 تحويل label
 def label_map(x):
     if x == "Reconnaissance":
-        return 0  # Bot
+        return 0   
     else:
-        return 1  # Human (تقريب)
+        return 1    
 
 df["label"] = df["mitre_attack_tactics"].apply(label_map)
 
-# 🧠 إنشاء features مشابهة لمشروعك
 df_final = pd.DataFrame()
 
 df_final["command_count"] = df["orig_pkts"]
@@ -36,7 +31,6 @@ df_final["unique_passwords"] = df["orig_bytes"]
 
 df_final["label"] = df["label"]
 
-# حفظ
 df_final.to_csv("data/processed/uwf_dataset.csv", index=False)
 
 print("✅ UWF dataset prepared")
