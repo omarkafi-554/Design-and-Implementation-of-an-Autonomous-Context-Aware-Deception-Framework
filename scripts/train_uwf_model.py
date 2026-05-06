ed7cf51c86cdfb193bfef9b79f8151119f51e6dc
omarkafi-554 <omar.kafi.2017@gmail.com>
2026-05-06 17:08:57 +0300

    Update train_uwf_model.py

--- scripts/train_uwf_model.py
+++ scripts/train_uwf_model.py
@@ -4,20 +4,14 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, confusion_matrix
 
-# ---------------------------
-# تحميل البيانات
-# ---------------------------
+
 df = pd.read_csv("data/processed/uwf_dataset.csv")
 
-# ---------------------------
-# Features و Label
-# ---------------------------
+
 X = df[["command_count", "login_attempts", "unique_passwords"]]
 y = df["label"]
 
-# ---------------------------
-# تقسيم البيانات
-# ---------------------------
+
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
@@ -26,9 +20,7 @@ print("✅ Data split done")
 print("Train size:", len(X_train))
 print("Test size:", len(X_test))
 
-# ---------------------------
-# تدريب الموديل (مع معالجة imbalance)
-# ---------------------------
+
 model = RandomForestClassifier(
     n_estimators=100,
     random_state=42,
@@ -39,29 +31,20 @@ model.fit(X_train, y_train)
 
 print("\n✅ Model trained")
 
-# ---------------------------
-# التوقع
-# ---------------------------
+
 y_pred = model.predict(X_test)
 
-# ---------------------------
-# Evaluation
-# ---------------------------
+
 print("\n📊 Classification Report:\n")
 print(classification_report(y_test, y_pred))
 
-# ---------------------------
-# Confusion Matrix
-# ---------------------------
+
 cm = confusion_matrix(y_test, y_pred)
 
 print("\n📊 Confusion Matrix:")
 print(cm)
 
-# ---------------------------
-# استخراج TP / TN / FP / FN
-# ---------------------------
-# نعتبر Bot (0) هو Positive
+
 TP = cm[0][0]
 FN = cm[0][1]
 FP = cm[1][0]
